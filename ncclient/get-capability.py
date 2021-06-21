58880c8231a56f426747346a7c46db4aaa166448
stefanoridolfi <stefano.ridolfi@gmail.com>
2021-06-21 10:22:16 +0200

    Update get-capability.py

--- ncclient/get-capability.py
+++ ncclient/get-capability.py
@@ -1,5 +1,5 @@
 #! /usr/bin/env python3
-
+host='1.1.1.1'
 from ncclient import manager
 with manager.connect(host=host, port=830, username='admin', password='admin',hostkey_verify=False) as m2:
     c2=m2.server_capabilities
