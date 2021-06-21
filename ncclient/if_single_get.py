77d320f1355b7d16631b7eb234196733e48eef19
stefanoridolfi <stefano.ridolfi@gmail.com>
2021-06-21 09:09:36 +0200

    Update if_single_get.py

--- ncclient/if_single_get.py
+++ ncclient/if_single_get.py
@@ -20,3 +20,18 @@ if_filter = '''<interface-configurations xmlns="http://cisco.com/ns/yang/Cisco-I
 with manager.connect(host=host, port=830, username='admin', password='admin',hostkey_verify=False) as m:
     c2=m.get_config(source=db, filter=('subtree', if_filter))
 print(c2)
+with manager.connect(host=host, port=830, username='admin', password='admin',hostkey_verify=False) as m:
+    c1=m.locked(target='candidate')
+        c2=m.edit_config(target="candidate",config=config_if4,default_operation='merge')
+    c3=m.validate()
+
+    if db=="running":
+        print(" edit in running")
+        c4=m.commit()
+
+print("edit candidate ",c2)
+print("validate ",c3)
+if db=="running":
+    print("commit ",c4)
+else:
+    print("Not commited")
