c88f1a7cc3752832f4c4773237dcb46aba60a46b
stefanoridolfi <stefano.ridolfi@gmail.com>
2021-06-21 09:06:28 +0200

    Delete get-capability.py

--- get-capability.py
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env python3
-
-from ncclient import manager
-with manager.connect(host=host, port=830, username='admin', password='admin',hostkey_verify=False) as m2:
-    c2=m2.server_capabilities
-for elem in c2:
-    print(elem)
